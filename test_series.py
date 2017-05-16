66e49efb5bb8a7c08fc682cd70f5e3b4daf4fb25
yanil3500 <yanil3500@gmail.com>
2017-05-16 14:28:46 -0700

    creates test for lucas-series function

--- test_series.py
+++ test_series.py
@@ -7,8 +7,17 @@ params_table = [
     (0, 0), (1, 0), (2, 1), (3, 1), (8, 13)
 ]
 
+lucas_params_table = [
+    (0, 2), (1, 2), (2, 1), (3, 3), (4, 4), (5, 7), (6, 11),
+]
+
 
 @pytest.mark.parametrize("n, result", params_table)
 def test_fibonacci(n, result):
     from series import fibonacci
     assert fibonacci(n) == result
+
+
+def test_lucas(n, result):
+    from series import lucas
+    assert lucas(n) == result
